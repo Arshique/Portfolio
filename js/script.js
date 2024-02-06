6b0a365ebffdfe724f088023bdf7accfa9305f57
Arshique Alam <alamarshique9@gmail.com>
2024-02-06 22:14:48 +0530

    added scroll reveal js.

--- js/script.js
+++ js/script.js
@@ -39,3 +39,16 @@ window.onscroll = () => {
     newIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
 };
+
+/*-------------------- SCROLL REVEAL -------------------- */
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content , .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img , .project-container , .portfolio-box , .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
